6e95ca9868ed1c05936e6657eb5202c831886639
mpesa21 <mpesa21@student.foi.hr>
2024-05-10 12:07:38 +0200

    Add Generic Repo

---
+++ PraksaHDmp/Contracts/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace PraksaHDmp.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetAdync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<bool> Exists(int id);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(T entity);
+        Task AddAsync(T entity);
+    }
+}
---
+++ PraksaHDmp/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using PraksaHDmp.Contracts;
+using PraksaHDmp.Data;
+
+namespace PraksaHDmp.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public Task<T> GetAdync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    
+}
